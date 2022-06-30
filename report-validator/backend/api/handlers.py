403a61f84f2eafe77916b4bf2f6693cc2262d945
Alexander Krutov <madrid211514@gmail.com>
2022-06-30 22:14:15 +0300

    Implemented 3 point of the task - rest #1 added 2 endpoints into handlers.py

--- report-validator/backend/api/handlers.py
+++ report-validator/backend/api/handlers.py
@@ -6,3 +6,105 @@ from fastapi.encoders import jsonable_encoder
 
 
 router = APIRouter()
+
+
+@router.post(
+    '/api/validation_report_multipart',
+    status_code=200,
+    response_model=ValidatorResponse,
+    responses={
+        200: {'model': ValidatorResponse},
+        400: {'model': ValidatorResponseBadRequest},
+        422: {'model': ValidatorResponse},
+        500: {'model': ValidatorResponse}
+    }
+)
+def report_validation_multipart(response: Response,
+                                report_description: ReportDescription = Body(
+                                    ...),
+                                file: UploadFile = File(...)
+                                ):
+    try:
+        validator_reports = ValidatorReports(report_description, file.filename)
+        if len(validator_reports.validation_report()) == 0:
+            validator_reports.save_report(file.file.read())
+            response.status_code = status.HTTP_200_OK
+            status_json = "Успешно"
+            parser = validator_reports.format_file
+            results = validator_reports.validation_report()
+
+        else:
+            status_json = "С ошибками"
+            parser = validator_reports.format_file
+            results = validator_reports.validation_report()
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+
+        return ValidatorResponse(
+            status=status_json,
+            parser=parser,
+            results=results
+        )
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return ValidatorResponse(
+            status='error',
+            parser=None,
+            results=[]
+        )
+
+
+@router.post('/api/validation_report',
+             status_code=200,
+             response_model=ValidatorResponse,
+             responses={
+                 200: {'model': ValidatorResponse},
+                 400: {'model': ValidatorResponseBadRequest},
+                 422: {'model': ValidatorResponse},
+                 500: {'model': ValidatorResponse}
+             })
+def report_validation(response: Response,
+                      report_description: ReportDescriptionWithLink = Body(...)
+                      ):
+    try:
+        file_uploader = FileUploader(report_description.report_link)
+        file = file_uploader.upload_file()
+        status_code_file_uploader = file['status_code']
+        if status_code_file_uploader != 200:
+            response.status_code = status_code_file_uploader
+            status_json = "Ошибка"
+            message = file['message']
+            parser = None
+            results = []
+            response_json = ValidatorResponseBadRequest(
+                status=status_json, message=message, parser=parser, results=results)
+            return JSONResponse(
+                status_code=status_code_file_uploader,
+                content=jsonable_encoder(response_json)
+            )
+
+        validator_reports = ValidatorReports(
+            report_description, file['filename'])
+        if len(validator_reports.validation_report()) == 0:
+            validator_reports.save_report(file['file_binary'])
+            response.status_code = status.HTTP_200_OK
+            status_json = "Успешно"
+            parser = validator_reports.format_file
+            results = validator_reports.validation_report()
+        else:
+            status_json = "С ошибками"
+            parser = validator_reports.format_file
+            results = validator_reports.validation_report()
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+
+        return ValidatorResponse(
+            status=status_json,
+            parser=parser,
+            results=results
+        )
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return ValidatorResponse(
+            status='error',
+            parser=None,
+            results=[]
+        )
