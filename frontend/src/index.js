377d55e233d1b638a3ec9af7544fc76089eacfa7
jiahao66666 <151733755+jiahao66666@users.noreply.github.com>
2025-07-18 06:35:55 +0800

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// 性能监控（可选）
+reportWebVitals();
