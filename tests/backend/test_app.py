ccc7212644e8e84f94b597eaf5d39f18e6263362
jiahao66666 <151733755+jiahao66666@users.noreply.github.com>
2025-07-18 20:59:29 +0800

    Update test_app.py

--- tests/backend/test_app.py
+++ tests/backend/test_app.py
@@ -3,7 +3,8 @@ import json
 import pytest
 from backend.app import app
 from werkzeug.datastructures import FileStorage
-
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 @pytest.fixture
 def client():
